45c65165ed3bbcea1163bd3f87c11a76d6f2eed3
Suleman <chintutudu2@gmail.com>
2023-11-21 11:19:51 +0530

    Sound setting in setting page

--- src/Screens/Main/Settings/helpers.ts
+++ src/Screens/Main/Settings/helpers.ts
@@ -1,4 +1,6 @@
+import Constants from '@constants/index';
 import {pop} from '@helpers/NavigatorHelper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import hapticFeedback from '@utils/HapticUtils';
 
 function onPressBack() {
@@ -6,4 +8,44 @@ function onPressBack() {
   pop();
 }
 
-export {onPressBack};
+async function onPressSound(
+  setIsSoundOn: React.Dispatch<React.SetStateAction<boolean>>,
+) {
+  hapticFeedback.triggerImpactHeavy();
+  const updatedSound = await updateSoundAsync();
+  setIsSoundOn(updatedSound);
+}
+async function updateSoundAsync(): Promise<boolean> {
+  try {
+    const existingSound = await AsyncStorage.getItem(Constants.AsyncKeys.sound);
+    if (existingSound == 'false') {
+      await AsyncStorage.setItem(Constants.AsyncKeys.sound, 'true');
+      return true;
+    } else {
+      await AsyncStorage.setItem(Constants.AsyncKeys.sound, 'false');
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+    return true;
+  }
+}
+
+// Update state in initial render
+async function getSoundSettingFromAsync(
+  setIsSoundOn: React.Dispatch<React.SetStateAction<boolean>>,
+) {
+  try {
+    const soundFlag = await AsyncStorage.getItem(Constants.AsyncKeys.sound);
+    if (soundFlag == undefined || soundFlag == null) {
+      await AsyncStorage.setItem(Constants.AsyncKeys.sound, 'true');
+      setIsSoundOn(true);
+    } else {
+      setIsSoundOn(soundFlag == 'true' ? true : false);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export {onPressBack, onPressSound, getSoundSettingFromAsync};
--- src/Screens/Main/Settings/index.tsx
+++ src/Screens/Main/Settings/index.tsx
@@ -1,14 +1,24 @@
 import Components from '@components/index';
-import React, {memo} from 'react';
+import React, {memo, useEffect, useState} from 'react';
 import {styles} from '@screens/Main/Settings/styles';
 import {Image, View} from 'react-native';
 import Constants from '@constants/index';
 import {ButtonIcon, ButtonText} from '@components/Buttons';
 import {RowView} from '@components/Flexs';
-import {onPressBack} from '@screens/Main/Settings/helpers';
+import {
+  getSoundSettingFromAsync,
+  onPressBack,
+  onPressSound,
+} from '@screens/Main/Settings/helpers';
 import {ImageContainer} from '@components/Containers';
 
 const Settings = memo(function Settings(props) {
+  const [isSoundOn, setIsSoundOn] = useState<boolean>(true);
+
+  useEffect(() => {
+    getSoundSettingFromAsync(setIsSoundOn);
+  }, []);
+
   return (
     <Components.Layouts.AppLayout
       background={Constants.Images.AppBackground_2}
@@ -21,7 +31,8 @@ const Settings = memo(function Settings(props) {
             style={styles.logo}
           />
         </ImageContainer>
-        <RowView style={styles.buttonContainer}>
+        {/* TODO: Add when server is added */}
+        {/* <RowView style={styles.buttonContainer}>
           <ButtonIcon
             style={styles.iconButton}
             isValid
@@ -36,22 +47,38 @@ const Settings = memo(function Settings(props) {
             Icon={<Image source={Constants.Images.PlusIcon} />}
           />
           <ButtonText isValid text={Constants.Strings.add_dare} />
-        </RowView>
+        </RowView> */}
         <RowView style={styles.buttonContainer}>
           <ButtonIcon
             style={styles.iconButton}
             isValid
-            Icon={<Image source={Constants.Images.MusicIcon} />}
+            Icon={
+              <Image
+                source={
+                  isSoundOn
+                    ? Constants.Images.SoundIcon
+                    : Constants.Images.SoundIconDisabled
+                }
+              />
+            }
+            onPress={() => onPressSound(setIsSoundOn)}
           />
-          <ButtonText isValid text={Constants.Strings.sound_on} />
+          <ButtonText
+            isValid
+            text={
+              isSoundOn
+                ? Constants.Strings.sound_on
+                : Constants.Strings.sound_off
+            }
+            onPress={() => onPressSound(setIsSoundOn)}
+          />
         </RowView>
         <RowView style={styles.buttonContainer}>
           <ButtonIcon
             style={styles.iconButton}
-            isValid
             Icon={<Image source={Constants.Images.ShareIcon} />}
           />
-          <ButtonText isValid text={Constants.Strings.share} />
+          <ButtonText text={Constants.Strings.share} />
         </RowView>
       </View>
       <RowView style={styles.backButton}>
